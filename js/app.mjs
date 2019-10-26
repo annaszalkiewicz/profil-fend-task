08640ba9c640537f40f082e03f794477c607dc3a
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-10-26 21:20:31 +0200

    Initiate view, model & controller objects Run controller.init() on load

--- js/app.mjs
+++ js/app.mjs
@@ -1,3 +1,9 @@
 import Model from './model.mjs';
 import View from './view.mjs';
 import Controller from './controller.mjs';
+
+const view = new View();
+const model = new Model();
+const controller = new Controller(view, model);
+
+controller.init();
