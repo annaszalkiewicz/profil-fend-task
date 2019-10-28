b59b57bfb8756e611c4b81aaeb2fde9d29d5a1ea
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-10-29 00:50:52 +0100

    Create render method that will display each card from search results

--- js/model.mjs
+++ js/model.mjs
@@ -1,5 +1,5 @@
 class Model {
-	constructor(title, url, image, status, releaseDate, rating, description, results) {
+	constructor(title, url, image, status, releaseDate, rating, description) {
 		this.title = title;
 		this.url = url;
 		this.image = image;
@@ -7,22 +7,7 @@ class Model {
 		this.releaseDate = releaseDate;
 		this.rating = rating;
 		this.description = description;
-		this.results = results;
 	}
-
-	fetchSeries = () => {
-		const input = document.getElementById('name');
-		let query = input.value.trim();
-
-		fetch(`https://api.tvmaze.com/search/shows?q=${query}`, {
-			method: 'GET'
-		})
-			.then(res => res.json())
-			.then(res => {
-				this.results = res;
-			})
-			.catch(err => console.log(err));
-	};
 }
 
 export default Model;
--- js/view.mjs
+++ js/view.mjs
@@ -2,19 +2,86 @@ class View {
 	constructor(value) {
 		this.value = value;
 	}
+	// Method that renders search results
+	render = result => {
+		const container = document.getElementById('results');
 
-	render() {}
+		/* Creates card container with link to page with full details */
+		const url = document.createElement('a');
+		url.setAttribute('href', `${result.url}`);
+		url.classList.add('card_container');
+		container.appendChild(url);
 
-	//Method that update input value
-	updateValue(e) {
+		// Creates image container
+		const imageContainer = document.createElement('div');
+		imageContainer.classList.add('image_container');
+		url.appendChild(imageContainer);
+
+		/* Creates image inside image container; if no image available, display image placeholder */
+		const image = document.createElement('img');
+		image.setAttribute(
+			'src',
+			result.image === null
+				? '../img/placeholder.jpg'
+				: `${result.image.medium}`
+		);
+		imageContainer.appendChild(image);
+
+		/* Creates details container that is sibling to image container */
+		const details = document.createElement('div');
+		details.classList.add('card_details');
+		url.appendChild(details);
+
+		/* Creates series title; if no title available display 'No title' instead */
+		const title = document.createElement('h2');
+		title.classList.add('card_title');
+		title.innerHTML = result.title ? `${result.title}` : 'No title';
+		details.appendChild(title);
+
+		/* Create series description; if no description available, display 'No description' */
+		const description = document.createElement('div');
+		description.classList.add('card_description');
+		details.appendChild(description);
+		description.innerHTML = result.description
+			? `${result.description}`
+			: 'No description';
+
+		// Creates more container
+		const more = document.createElement('div');
+		more.classList.add('card_more');
+		details.appendChild(more);
+
+		/* Display release date; if no available, display 'Unknown release date' */
+		const date = document.createElement('p');
+		date.classList.add('card_date');
+		more.appendChild(date);
+		date.innerHTML = result.releaseDate
+			? `${result.releaseDate}`
+			: 'Unknown release date';
+
+		// Display status; if no available, display 'Unknown status'
+		const status = document.createElement('p');
+		status.classList.add('card_status');
+		more.appendChild(status);
+		status.innerHTML = result.status ? `${result.status}` : 'Unknown status';
+
+		// Display rating; if no available, display 'No rating yet'
+		const rating = document.createElement('p');
+		rating.classList.add('card_rating');
+		more.appendChild(rating);
+		rating.innerHTML = result.rating ? `${result.rating}` : 'No rating yet';
+	};
+
+	// Method that update input value
+	updateValue = e => {
 		this.value = e.target.value;
-	}
+	};
 
 	// Display year in the footer
-	displayYear() {
+	displayYear = () => {
 		const d = new Date();
 		return (document.getElementById('footer_date').innerHTML = d.getFullYear());
-	}
+	};
 }
 
 export default View;
