db18ecf92e64c458150c1ea647c2b807cf3d6c4b
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-10-29 00:52:27 +0100

    Add showResults method This method map over results and call render method for each result

--- js/controller.mjs
+++ js/controller.mjs
@@ -1,7 +1,10 @@
+import Model from './model.mjs';
+
 class Controller {
-	constructor(view, model, results) {
+	constructor(view, model) {
 		this.view = view;
 		this.model = model;
+		this.results = [];
 
 		/* Event that listens to input change value & call updateValue method */
 		document
@@ -22,14 +25,47 @@ class Controller {
 		if (input.value.trim() === '') {
 			return;
 		} else {
-			this.model.fetchSeries();
+			this.fetchSeries();
 		}
+	};
+
+	fetchSeries = () => {
+		const input = document.getElementById('name');
+		let query = input.value.trim();
+
+		fetch(`https://api.tvmaze.com/search/shows?q=${query}`, {
+			method: 'GET'
+		})
+			.then(res => res.json())
+			.then(res => {
+				for (let i = 0; i < res.length; i++) {
+					this.results = [
+						...this.results,
+						new Model(
+							res[i].show.name,
+							res[i].show.url,
+							res[i].show.image,
+							res[i].show.status,
+							res[i].show.premiered,
+							res[i].show.rating.average,
+							res[i].show.summary
+						)
+					];
+				}
+
+				this.showResults();
+			})
+			.catch(err => console.log(err));
+	};
+
+	showResults = () => {
+		this.results.map(result => this.view.render(result));
 	};
 
 	init = () => {
 		this.view.displayYear();
 		console.log(this.view);
-		console.log(this.model);		
+		console.log(this.model);
 	};
 }
 
