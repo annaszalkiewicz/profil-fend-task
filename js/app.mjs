6daed8d2c2ca1d5f4d637933c09fd7211d27f8e9
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-11-03 00:35:21 +0100

    Call init method only when window is loaded

--- js/app.mjs
+++ js/app.mjs
@@ -2,8 +2,10 @@ import Model from './model.mjs';
 import View from './view.mjs';
 import Controller from './controller.mjs';
 
-const view = new View('');
-const model = new Model();
-const controller = new Controller(view, model);
+window.onload = () => {
+	const view = new View('');
+	const model = new Model();
+	const controller = new Controller(view, model);
 
-controller.init();
+	controller.init();
+};
