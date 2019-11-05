5ee51622fd39ccd47b33be905eeaf6be3a26a146
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-11-05 07:23:34 +0100

    Add autoprefixer task

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,31 @@ const gulp = require('gulp');
 const bs = require('browser-sync').create();
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 function serve() {
 	bs.init({
 		server: {
 			baseDir: './'
 		},
-		port: 8000,
-		ui: {
-			port: 8001
-		}
+		port: 8002
 	});
 }
 
 function style() {
 	return gulp
 		.src('./scss/**/*.scss')
-		.pipe(sass())
+		.pipe(sourcemaps.init())
+		.pipe(sass({ outputStyle: 'compressed' }))
+		.pipe(sourcemaps.write())
 		.pipe(concat('styles.css'))
+		.pipe(autoprefix())
 		.pipe(gulp.dest('./css'))
 		.pipe(bs.stream());
 }
 function scripts() {
-	return gulp
-		.src(['./js/app.mjs'])
-		.pipe(gulp.dest('./js'));
+	return gulp.src(['./js/app.mjs']).pipe(gulp.dest('./js'));
 }
 
 function defaultTask() {
@@ -37,6 +37,19 @@ function defaultTask() {
 	gulp.watch('./js/**/*.mjs').on('change', bs.reload);
 }
 
+function autoprefix() {
+	return gulp
+		.src('./css/styles.css')
+		.pipe(
+			autoprefixer({
+				browsers: ['> 0.5%', 'last 2 versions', 'not dead'],
+				cascade: false
+			})
+		)
+		.pipe(gulp.dest('build'));
+}
+
+exports.serve = serve;
 exports.style = style;
 exports.scripts = scripts;
 exports.default = defaultTask;
